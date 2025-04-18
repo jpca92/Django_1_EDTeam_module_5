16b7f75ed68b638941f3bf2a4bf4bdc59fcfc78b
jpca92 <jpca92@gmail.com>
2025-04-17 22:06:46 -0500

    lesson 5_1 completed

--- edteam_1_3/urls.py
+++ edteam_1_3/urls.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
 from rest_framework_simplejwt.views import(
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
     TokenBlacklistView
 )
+
+# This is a view that generates the schema for the API documentation using drf_yasg
+schema_view = get_schema_view(
+   openapi.Info(
+      title="POS Api",
+      default_version='v1',
+      description="Point of Sale API Rest",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api_1_3.urls')),
@@ -15,6 +36,8 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/token/verify/', TokenVerifyView.as_view()),
     path('api/token/blacklist/', TokenBlacklistView.as_view()),
-
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
